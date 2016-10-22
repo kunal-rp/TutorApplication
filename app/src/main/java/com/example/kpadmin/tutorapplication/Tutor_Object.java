94e8ebf1370338a4335225efd6f464580c35d385
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-22 11:58:22 -0700

    Created Basic Tutor Object

---
+++ app/src/main/java/com/example/kpadmin/tutorapplication/Tutor_Object.java
@@ -0,0 +1,13 @@
+package com.example.kpadmin.tutorapplication;
+
+/**
+ * Created by KP Admin on 10/22/2016.
+ */
+
+public class Tutor_Object  {
+
+    private String  Full_Name;
+    private double Rating;
+
+
+}
--- app/src/main/java/com/example/kpadmin/tutorapplication/Tutors_for_Class_Adapter.java
+++ app/src/main/java/com/example/kpadmin/tutorapplication/Tutors_for_Class_Adapter.java
@@ -1,8 +1,10 @@
 package com.example.kpadmin.tutorapplication;
 
+import android.widget.ArrayAdapter;
+
 /**
  * Created by KP Admin on 10/22/2016.
  */
 
-public class Tutors_for_Class_Adapter extends  {
+public class Tutors_for_Class_Adapter extends ArrayAdapter<>{
 }
