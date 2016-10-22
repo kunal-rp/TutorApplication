07db60c40d175513a290356c523866242f3d593b
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-22 11:48:04 -0700

    Created List Activity the layout will be reused for ANY list activity that just have list working on adapter for tutor overview object

---
+++ app/src/main/java/com/example/kpadmin/tutorapplication/Tutors_for_Class_Adapter.java
@@ -0,0 +1,8 @@
+package com.example.kpadmin.tutorapplication;
+
+/**
+ * Created by KP Admin on 10/22/2016.
+ */
+
+public class Tutors_for_Class_Adapter extends  {
+}
---
+++ app/src/main/java/com/example/kpadmin/tutorapplication/Tutors_for_Course.java
@@ -0,0 +1,13 @@
+package com.example.kpadmin.tutorapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tutors_for_Course extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_activity);
+    }
+}
